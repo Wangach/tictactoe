e413bb3c4da4e5c2cde14840867fa83a4e29b9ea
John Kimani <jwangach@gmail.com>
2021-06-16 19:59:21 +0300

    td-identity

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ const allTds = document.querySelectorAll('td');
 //load game UI function
 window.addEventListener('load', loadGame);
 
+//game functions
+gameHolder.addEventListener("click", showSth);
+
 
 
 //Function List
 function loadGame() {
 	gameHolder.style.display = 'block';
+}
+
+function showSth(e){
+	console.log(e);
 }
